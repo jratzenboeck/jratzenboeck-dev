3ba0b450dad012600c20f4baba604861a5c15dbe
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2020-10-24 19:46:36 +0200

    Add tailwind config + start with header and intro

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center p-2">
+    <h2 class="text-2xl">jratzenboeck</h2>
+    <nav>
+      <ul>
+        <li class="inline-block mr-4">
+          <nuxt-link to="/articles">Articles</nuxt-link>
+        </li>
+        <li class="inline-block mr-4">
+          <nuxt-link to="/projects">Projects</nuxt-link>
+        </li>
+        <li class="inline-block">
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ components/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-3 max-w-lg mx-auto text-center">
+    <h1 class="text-4xl my-3">Hi, I'm Jürgen</h1>
+    <img
+      src="~/assets/img/profile.jpg"
+      alt="Profile picture"
+      class="block h-32 w-32 rounded-full mx-auto"
+    />
+    <p class="my-3">
+      I care about crafting high quality software. Lorem ipsum dolor sit amet,
+      consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+      labore et dolore magna aliquyam erat, sed diam voluptua.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
