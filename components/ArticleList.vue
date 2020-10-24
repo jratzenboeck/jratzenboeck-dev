dc52b0f55a0591ca702f5d17788247ffb9cdecf5
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2020-10-24 22:35:53 +0200

    Start to add article list

---
+++ components/ArticleList.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="max-w-lg mx-auto p-2">
+    <ul>
+      <li v-for="story in stories" :key="story.id">
+        <h3 class="text-lg">{{ story.name }}</h3>
+        <span class="text-sm mr-4">Created at: {{ story.created_at }}</span>
+        <span class="text-sm"
+          >Reading time: {{ story.content.reading_time }}</span
+        >
+        <p>{{ story.content.content }}</p>
+        <button>Read more</button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    try {
+      const response = await context.app.$storyapi.get('cdn/stories', {
+        version: 'draft',
+        starts_with: 'articles/',
+      })
+      return response.data
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  data() {
+    return {
+      stories: [],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
--- plugins/components.js
+++ plugins/components.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Page from '~/components/Page.vue'
 import Intro from '~/components/Intro.vue'
+import ArticleList from '~/components/ArticleList.vue'
 
 Vue.component('page', Page)
 Vue.component('intro', Intro)
+Vue.component('article_list', ArticleList)
